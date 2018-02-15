5c3a5981316278b63e43f360eddfce416bc45aa6
Branden Mosley <branden.mosley1337@gmail.com>
2018-02-15 19:38:37 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -10,7 +10,8 @@ class Anagram
     #we want to return an empty array for no matches
     #or return an array including matches
     #iterate over the array passed to match
-    word_list.
+    word_list.each do |word|
+      word.split("").sort
   end
     
 end
